da6cbc359ad700e3781a400f5123888aa2f8b3eb
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2022-02-22 20:52:18 +0100

    added declarations for function members

--- DS/TREES/avl_tree.hpp
+++ DS/TREES/avl_tree.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <memory>
+#include "iterator.hpp"
 
 template<typename Key, typename Value>
 class AVLTree{};
@@ -13,15 +14,44 @@ class AVLNode{
 		friend class AVLTree<Key,Value>;
 
 		std::pair<Key,Value> elem;
-		left_child;
-		right_child;
+		std::shared_ptr<AVLNode<Key,Value>> left_child;
+		std::shared_ptr<AVLNode<Key,Value>> right_child;
 		std::weak_ptr<AVLNode<Key,Value>> parent;
 	protected:
 		size_t height;
 	private:
-		size_t getHeight(){ return height; }
-		size_t setHeight(size_t h) { height+= h; }
+		size_t setHeight(size_t h) { height = h; }
+};
 
+template<typename Key, typename Value>
+class AVLTree{
+	public:
+		using key_type = Key;
+		using value_type = Value;
+		using iterator = Iterator<std::shared_ptr<AVLNode<Key,Value>>>;
+		using node_type = AVLNode<Key,Value>
+		
+		AVLTree();
+		constexpr size_t size() const noexcept { return sz; }
+		constexpr bool is_empty() const noexcept { return size() == 0; }
+		void insert(const std::pair<Key,Value>&);
+		void remove(const Key&);
+		void remove(iterator&);
+		
+		iterator find(const Key&);
+		iterator begin();
+		iterator end();
+	protected:
+		void __insert(std::shared_ptr<node_type>&, std::shared_ptr<node_type>&);
+		std::shared_ptr<node_type>& __search(std::shared_ptr<node_type>& const Key&);
+		void setHeight(std::shared_ptr<node_type>&);
+		size_t height(std::shared_ptr<node_type>&);
+		bool isBalanced(std::shared_ptr<node_type>&);
+		std::shared_ptr<node_type>& tallGrandchild(std::shared_ptr<node_type>&);
+		void rebalance(std::shared_ptr<node_type>&);
+	private:
+		constexpr size_t sz; // size of tree
+		std::shared_ptr<node_type> root_node, super_root_node;
 };
 
 #endif // _MY_AVL_TREE
