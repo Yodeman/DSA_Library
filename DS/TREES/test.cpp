e32af19b86d365f33dfe04cc0ca7318b7b508c38
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2022-02-26 18:22:10 +0100

    added test samples for avl tree.

--- DS/TREES/test.cpp
+++ DS/TREES/test.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <random>
-#include <memory>
-#include "./binary_tree.hpp"
-#include "binary_search_tree.hpp"
+//#include <random>
+//#include <memory>
+//#include "./binary_tree.hpp"
+//#include "binary_search_tree.hpp"
+#include "avl_tree.hpp"
+
 /*
 void foo1()
 {
@@ -32,19 +34,31 @@ void foo()
 	if (sp2) std::cout << *sp2 << std::endl;
 }
 */
-/*
-void bst_test()
+
+void tree_test()
 {
-	SearchTree<std::string, int> s{};
+	/*
+	//SearchTree<std::string, int> s{};
+	AVLTree<std::string, int> s{};
 	s.insert(std::make_pair<std::string, int>("Paul", 20));
 	s.insert(std::make_pair<std::string, int>("Peter", 16));
 	s.insert(std::make_pair<std::string, int>("Lydia", 18));
 	s.insert(std::make_pair<std::string, int>("Mary", 22));
 	s.insert(std::make_pair<std::string, int>("Emmanuel", 10));
 	s.insert(std::make_pair<std::string, int>("Nathaniel", 20));
-
+	s.insert(std::make_pair<std::string, int>("Paul", 21));
 	std::cout << s.size() << std::endl;
-	
+	*/
+	AVLTree<int, char> s{};
+	s.insert(std::make_pair<int, char>(44, 'a'));
+	s.insert(std::make_pair<int, char>(17, 'b'));
+	s.insert(std::make_pair<int, char>(78, 'c'));
+	s.insert(std::make_pair<int, char>(32, 'd'));
+	s.insert(std::make_pair<int, char>(50, 'e'));
+	s.insert(std::make_pair<int, char>(88, 'f'));
+	s.insert(std::make_pair<int, char>(48, 'g'));
+	s.insert(std::make_pair<int, char>(62, 'h'));
+	s.insert(std::make_pair<int, char>(54, 'f'));
 	for(auto& i : s)
 		std::cout << "key: " <<i.first << ", value: " << i.second << std::endl;
 	
@@ -54,13 +68,16 @@ void bst_test()
 	//	++i;
 	//}
 	
-	auto iter = s.find("Nathaniel");
+	//auto iter = s.find("Nathaniel");
+	auto iter = s.find(62);
 	std::cout << (*iter).first << " " << (*iter).second << std::endl;
 	
 	try{
-		s.remove("Paul");
+		//s.remove("Paul");
+		s.remove(32);
 		std::cout << s.size() << std::endl;
-		s.remove("Victor");
+		//s.remove("Victor");
+		s.remove(90);
 	}
 	catch(std::exception& e){
 		std::cerr << e.what() << std::endl;
@@ -71,8 +88,8 @@ void bst_test()
 	for(auto& i : s)
 		std::cout << "Key: " << i.first << ", Value: " << i.second << std::endl;
 }
-*/
 
+/*
 void avl_node_test()
 {
 	struct Node{
@@ -92,9 +109,10 @@ void avl_node_test()
 	r_child->parent = node;
 	std::shared_ptr<Node> node2 = std::make_shared<Node>(5);
 }
+*/
 int main()
 {
 	//foo();
-	//bst_test();
-	avl_node_test();
+	tree_test();
+	//avl_node_test();
 }
