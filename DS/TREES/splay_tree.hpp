17d7239fc94b8219d92646ab715dac2f2729149f
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2022-03-26 09:02:18 +0100

    changed the implementation of public find member function to use an iterative approach instead of the previous recursive approach
    this approach enables the function to splay the parent of the external node if the key isn't found in the tree.

--- DS/TREES/splay_tree.hpp
+++ DS/TREES/splay_tree.hpp
@@ -190,14 +190,38 @@ void SplayTree<Key,Value>::remove(const Key& key)
 	throw std::runtime_error("entry with key doest not exists in the tree!!!");
 }
 
+/*
+ * Search for key in the tree, if the key is found at a node,
+ * splaying is performed on the node, else the parent of the
+ * external node at which the search terminates unsuccessfully
+ * is splayed.
+*/ 
 template<std::totally_ordered Key, typename Value>
 typename SplayTree<Key,Value>::iterator SplayTree<Key,Value>::find(const Key& key)
 {
-	auto node = __search(root_node, key);
-	if (!node)
+	auto node = root_node;
+	std::shared_ptr<SplayTreeNode<Key,Value>> parent;
+	while(node) {
+		if (key < (node->elem).first) {
+			parent_node = node;
+			node = node->left_child;
+		}
+		else if (key > (node->elem).first) {
+			parent_node = node;
+			node = node->right_child;
+		}
+		else
+			break;
+	}
+	if (node && ((node->elem).first == key)){
+		restructure(node);
+		SplayTree<Key,Value>::iterator iter(node);
+		return iter;
+	}
+	else {
+		restructure(parent_node)
 		return end();
-	SplayTree<Key,Value>::iterator iter(node);
-	return iter;
+	}
 }
 
 template<std::totally_ordered Key, typename Value>
