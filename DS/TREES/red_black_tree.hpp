a03cdbbc3432df562d5fcc062eb9aa12510781aa
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2022-04-06 14:43:34 +0100

    added definition for constructor.

--- DS/TREES/red_black_tree.hpp
+++ DS/TREES/red_black_tree.hpp
@@ -22,21 +22,33 @@ class RBTree{
 		using value_type = Value;
 		using iterator = Iterator<std::shared_ptr<RBTreeNode<Key,Value>>>;
 
+		RBTree();
+
 		size_t size() const noexcept { return sz; }
 		bool is_empty() const noexcept { return sz==0; }
 
-		void insert(const Key& key, const Value& value);
-		void remove(const Key& k);
-		iterator find(const Key& k);
+		void insert(const Key&, const Value&);
+		void remove(const Key&);
+		iterator find(const Key&);
 		iterator begin();
 		iterator end();
 
 	protected:
 		using node_type = std::shared_ptr<RBTreeNode<Key,Value>>;
+		void __insert(std::shared_ptr<node_type>&, std::shared_ptr<node_type>&);
+		std::shared_ptr<node_type>& __search(std::shared_ptr<node_type>&, const Key&);
 	private:
 		size_t sz;
 		node_type root_node, super_root_node;
 };
+
+template<std::totally_ordered Key, typename Value>
+RBTree<Key,Value>::RBTree() : sz{0} {
+	root_node = std::make_shared<RBTree<Key,Value>::node_type>();
+	super_root_node = std::make_shared<RBTree<Key,Value>::node_type>();
+	root_node->parent = super_root_node;
+	super_root_node->left_child = root_node;
+}
 
 
 #endif //MY_RB_TREE
