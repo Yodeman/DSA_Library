9f933d8d4d809afe37affa5c139234c1af37f868
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2022-04-10 15:33:08 +0100

    initial definition of merge sort algorithm.

--- algorithms/sorting/merge_sort.hpp
+++ algorithms/sorting/merge_sort.hpp
@@ -3,15 +3,26 @@
 
 #include <iostream>
 #include <ranges>
+#include <cmath>
 
 template<typename Container>
 concept sort_requirement = std::ranges::range<Container> &&\
-						   std::totally_ordered<Container::value_type>
+						   std::totally_ordered<std::ranges::range_value_t<Container>>;
 
-template<sort_requirement Container, std::predicate Compare=std::less<Container::value_type>>
-void merge_sort(Container C, Compare comp)
+template<
+		sort_requirement Container,
+		std::predicate Compare=std::less<Container::value_type>
+	>
+void merge_sort(Container& C, Compare comp)
 {
-	// pass...
+	size_t sz = std::ranges::size(C);
+	if (sz == 1) return;
+	size_t n = sz / 2;
+	auto s1 = std::ranges::subrange(C.begin(), C.begin()+n);
+	auto s2 = std::ranges::subrange(C.beigin()+n, C.end());
+	merge_sort(s1, comp);
+	merge_sort(s2, comp);
+	std::ranges::merge(s1, s2, C.begin());
 }
 
 #endif // MY_MERGE_SORT
