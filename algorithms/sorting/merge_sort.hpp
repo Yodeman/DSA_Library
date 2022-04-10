821f79eeb272e178b1e0f617d89a3103e9ade864
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2022-04-10 21:32:14 +0100

    added implementation for merge operation

--- algorithms/sorting/merge_sort.hpp
+++ algorithms/sorting/merge_sort.hpp
@@ -2,6 +2,7 @@
 #define MY_MERGE_SORT
 
 #include <iostream>
+#include <iterator>
 #include <ranges>
 #include <cmath>
 
@@ -11,18 +12,58 @@ concept sort_requirement = std::ranges::range<Container> &&\
 
 template<
 		sort_requirement Container,
-		std::predicate Compare=std::less<Container::value_type>
+		std::forward_iterator Iter,
+		typename Compare = std::less<std::ranges::range_value_t<Container>>
 	>
-void merge_sort(Container& C, Compare comp)
+void my_merge(Container& C1, Container& C2, Iter O, Compare comp = {})
+{
+	auto iter1 = C1.begin();
+	auto iter2 = C2.begin();
+	while ((iter1 != C1.end()) && (iter2 != C2.end())) {
+		if (comp(*iter1, *iter2)) {
+			*O = *iter1;
+			++iter1;
+		} else {
+			*O = *iter2;
+			++iter2;
+		}
+		++O;
+	}
+	while (iter1 != C1.end()) {
+		*O = *iter1;
+		++iter1;
+		++O;
+	}
+	while (iter2 != C2.end()) {
+		*O = *iter2;
+		++iter2;
+		++O;
+	}
+}
+
+template<
+		sort_requirement Container,
+		typename Compare = std::less<std::ranges::range_value_t<Container>>,
+		typename Elem_type = std::ranges::range_value_t<Container>
+		//std::predicate Compare = std::less<std::ranges::range_value_t<Container>>
+	>
+void merge_sort(
+			Container& C,
+			Compare comp = {}
+		)
 {
 	size_t sz = std::ranges::size(C);
 	if (sz == 1) return;
 	size_t n = sz / 2;
-	auto s1 = std::ranges::subrange(C.begin(), C.begin()+n);
-	auto s2 = std::ranges::subrange(C.beigin()+n, C.end());
+	std::vector<Elem_type> s1(n);
+	s1.shrink_to_fit();
+	std::vector<Elem_type> s2(sz-n);
+	s2.shrink_to_fit();
+	std::ranges::copy(std::ranges::subrange(C.begin(), C.begin()+n), s1.begin());
+	std::ranges::copy(std::ranges::subrange(C.begin()+n, C.end()), s2.begin());
 	merge_sort(s1, comp);
 	merge_sort(s2, comp);
-	std::ranges::merge(s1, s2, C.begin());
+	my_merge(s1, s2, C.begin(), comp);
 }
 
 #endif // MY_MERGE_SORT
