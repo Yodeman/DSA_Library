5a84089e9efa2ab6411c498e98e52576b9b23ed8
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2022-04-11 15:23:47 +0100

    added a general interface to call sort on any container
    commented out part that gives verbose output.

--- algorithms/sorting/sort_test.cpp
+++ algorithms/sorting/sort_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include <vector>
 #include <valarray>
 #include <list>
@@ -19,96 +20,85 @@ void print(Container& C)
 	std::cout << std::endl;
 }
 
+template<std::ranges::range Container>
+void deco(Container& C)
+{
+	//std::cout << "\tBefore: ";
+	//print(C);
+	auto start = std::chrono::steady_clock::now();
+	merge_sort(C);
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	//std::cout << "\tAfter: ";
+	//print(C);
+	std::cout << "\telapsed time: " << elapsed_seconds.count() <<"s\n\n";
+}
+
 int main()
 {
+	constexpr unsigned int sz = 100000;
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_int_distribution<>dis(1, 100);
-	std::array<int, 21> a {
-			30, 32, 3, 46, 90, 10, 1, 0, 88, 67, 77, 5, 2, 1,
-			29, 10, 18, 86, 54, 43, 50
-		};
-	std::vector<int> v;
-	std::deque<int> d;
-	std::forward_list<int> f;
-	std::list<int> l;
-	std::unordered_set<int> us;
-	std::stack<int> s;
-	std::queue<int> q;
-	std::priority_queue<int> pq;
+	std::uniform_int_distribution<>dis(1, 1000);
+	//std::array<int, 21> a {
+	//		30, 32, 3, 46, 90, 10, 1, 0, 88, 67, 77, 5, 2, 1,
+	//		29, 10, 18, 86, 54, 43, 50
+	//	};
+	std::array<int, sz> a;
+	std::vector<int> v(sz);
+	v.shrink_to_fit();
+	std::deque<int> d(sz);
+	d.shrink_to_fit();
+	std::forward_list<int> f(sz);
+	std::list<int> l(sz);
+	//std::unordered_set<int> us;
+	//std::stack<int> s;
+	//std::queue<int> q;
+	//std::priority_queue<int> pq;
 
-	for (auto i=0; i < 21; ++i) {
+	for (size_t i=0; i < 100000; ++i) {
 		auto j = dis(gen);
+		a.at(i) = j;
 		v.push_back(j);
 		d.push_back(j);
 		l.push_back(j);
-		us.emplace(j);
-		s.emplace(j);
-		q.emplace(j);
-		pq.emplace(j);
+		//us.emplace(j);
+		//s.emplace(j);
+		//q.emplace(j);
+		//pq.emplace(j);
 		f.push_front(j);
 	}
 	
-	std::cout << "Test with std::vector:\n";
-	std::cout << "\tBefore: ";
-	print(v);
-	merge_sort(v);
-	std::cout << "\tAfter: ";
-	print(v);
+	std::cout << "Test with std::array\n";
+	deco(a);
+
+	std::cout << "Test with std::vector\n";
+	deco(v);
 
 	std::cout << "Test with std::deque:\n";
-	std::cout << "\tBefore: ";
-	print(d);
-	merge_sort(d);
-	std::cout << "\tAfter: ";
-	print(d);
+	deco(d);
 
 	std::cout << "Test with std::list:\n";
-	std::cout << "\tBefore: ";
-	print(l);
-	merge_sort(l);
-	std::cout << "\tAfter: ";
-	print(l);
+	deco(l);
 
-	/*std::cout << "Test with std::unordered_set:\n";
-	std::cout << "\tBefore: ";
-	print(us);
-	merge_sort(us);
-	std::cout << "\tAfter: ";
-	print(us);*/
+	//std::cout << "Test with std::unordered_set:\n";
+	//deco(us);
 
 	std::cout << "Test with std::forward_list:\n";
-	std::cout << "\tBefore: ";
-	print(f);
-	merge_sort(f);
-	std::cout << "\tAfter: ";
-	print(f);
+	deco(f);
 	
-	/*
+	
 	// uncommenting this part will cause compilation to fail,
 	// since the containers do not satisfy the requirements
 	// of the algorithm.
-	std::cout << "Test with std::stack:\n";
-	std::cout << "\tBefore: ";
-	print(s);
-	//merge_sort(s);
-	std::cout << "\tAfter: ";
-	print(s);
+	//std::cout << "Test with std::stack:\n";
+	//deco(s);
 
-	std::cout << "Test with std::queue:\n";
-	std::cout << "\tBefore: ";
-	print(q);
-	//merge_sort(q);
-	std::cout << "\tAfter: ";
-	print(q);
+	//std::cout << "Test with std::queue:\n";
+	//deco(q);
 
-	std::cout << "Test with std::priority_queue:\n";
-	std::cout << "\tBefore: ";
-	print(pq);
-	//merge_sort(pq);
-	std::cout << "\tAfter: ";
-	print(pq);
-	*/
+	//std::cout << "Test with std::priority_queue:\n";
+	//deco(pq);
 
 	return 0;
 }
