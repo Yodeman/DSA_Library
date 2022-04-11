b8ea12c7dec04c116dbfc52e09fb74c00be82737
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2022-04-11 16:44:24 +0100

    added iterator concept

--- algorithms/sorting/algorithms_concept.hpp
+++ algorithms/sorting/algorithms_concept.hpp
@@ -9,4 +9,7 @@ template<typename Container>
 concept sort_requirement = std::ranges::range<Container> &&\
 						   std::totally_ordered<std::ranges::range_value_t<Container>>;
 
+template<typename Iter>
+concept iterator_requirement = std::output_iterator<Iter,std::iter_value_t<Iter>>;
+
 #endif // MY_ALGORITHM_CONCEPT
